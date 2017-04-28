42fbd3123413e7c1531396147490e99bcb1bc834
qingw <qingw@jumei.com>
2017-04-28 09:51:22 +0800

    1. 添加了 codecov 的徽章， 为了演示方便，暂时启用 codecov。让大家有一个宏观的认识。

---
+++ app/src/main/java/gdky005/androidbadge/L.java
@@ -0,0 +1,37 @@
+package gdky005.androidbadge;
+
+import android.util.Log;
+
+/**
+ * Created by WangQing on 2017/4/25.
+ */
+
+public class L {
+    private static final String TAG = "L";
+
+    private static L instance = null;
+
+    private L() {
+    }
+
+    public static L getInstance() {
+        return new L();  //这里为了玩玩  百分百的 代码覆盖率，随便写的。
+
+        //正确写法
+//            if (instance == null) {
+//                synchronized (L.class) {
+//                    L temp = instance;
+//                    if (temp == null) {
+//                        temp = new L();
+//                        instance = temp;
+//                    }
+//                }
+//            }
+//            return instance;
+    }
+
+
+    public static void i(String msg) {
+        Log.i(TAG, "i: " + msg);
+    }
+}
